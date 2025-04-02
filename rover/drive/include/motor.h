0707011a2c8bb694e249e76c9e77ae1ff1b71176
Etienne Naude <eti@naude.dev>
2025-04-02 18:21:58 +0100

    rewrite

---
+++ rover/drive/include/motor.h
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+class Motor
+{
+public:
+    int pinA = 7;
+    int pinB = 5;
+    int dataPin = 9;
+
+    Motor(int pinA, int pinB, int dataPin)
+    {
+        this->pinA = pinA;
+        this->pinB = pinB;
+        this->dataPin = dataPin;
+
+        pinMode(pinA, OUTPUT);
+        pinMode(pinA, OUTPUT);
+    }
+
+    void move(int speed = 50, bool direction = true)
+    {
+        digitalWrite(pinA, direction);
+        digitalWrite(pinB, !direction);
+
+        analogWrite(dataPin, speed);
+    }
+
+    void stop()
+    {
+        digitalWrite(pinA, LOW);
+        digitalWrite(pinB, LOW);
+        analogWrite(dataPin, 0);
+    }
+
+    void moveFor(int speed, bool direction, int time)
+    {
+        move(speed, direction);
+        delay(time);
+        stop();
+    }
+};
--- rover/drive/src/main.cpp
+++ rover/drive/src/main.cpp
@@ -1,133 +1,94 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#define IN1R 2
-#define IN2R 1
-#define IN3R 41
-#define IN4R 42
-#define ENAR 39
-#define ENBR 40
-#define IN1L 4
-#define IN2L 5
-#define IN3L 6
-#define IN4L 7
-#define ENAL 15
-#define ENBL 16
+#include <motor.h>
 
-const char* ssid = "Rees";
-const char* password = "password42";
+Motor rightA(1, 2, 39);
+Motor rightB(41, 42, 40);
+Motor leftA(4, 5, 15);
+Motor leftB(5, 6, 16);
 
-
-void MoveRightSide(int speed, bool dir){ //dir True = fwd, false = back
-  digitalWrite(IN1R, dir);
-  digitalWrite(IN2R, !dir);
-  digitalWrite(IN3R, dir);
-  digitalWrite(IN4R, !dir);
-  analogWrite(ENAR, speed);
-  analogWrite(ENBR, speed);
-  analogWrite(ENAL, speed);
-  analogWrite(ENBL, speed);
+void MoveRightSide(int speed, bool dir)
+{
+  rightA.move(speed, dir);
+  rightB.move(speed, dir);
 }
 
-void MoveLeftSide(int speed, bool dir){
-  digitalWrite(IN1L, dir);
-  digitalWrite(IN2L, !dir);
-  digitalWrite(IN3L, dir);
-  digitalWrite(IN4L, !dir);
-  analogWrite(ENAR, speed);
-  analogWrite(ENBR, speed);
-  analogWrite(ENAL, speed);
-  analogWrite(ENBL, speed);
+void MoveLeftSide(int speed, bool dir)
+{
+  leftA.move(speed, dir);
+  leftB.move(speed, dir);
 }
 
-void MoveFwd(int speed){ // side = True, right wheels; dir = True, forwards
+void MoveFwd(int speed)
+{
   MoveRightSide(speed, true);
   MoveLeftSide(speed, true);
 }
 
-void MoveBack(int speed){
+void MoveBack(int speed)
+{
   MoveRightSide(speed, false);
   MoveLeftSide(speed, false);
 }
 
-void MoveRight(int speed){
+void MoveRight(int speed)
+{
   MoveRightSide(speed, false);
   MoveLeftSide(speed, true);
 }
 
-void MoveLeft(int speed){
+void MoveLeft(int speed)
+{
   MoveRightSide(speed, true);
   MoveLeftSide(speed, false);
 }
 
-void Stop(){
+void Stop()
+{
   MoveRightSide(0, true);
   MoveLeftSide(0, true);
 }
 
 int defaultSpeed = 255;
-WiFiClient client;
 
-void setup() {
-  
-  // put your setup code here, to run once:
+void setup()
+{
   Serial.begin(115200);
-  pinMode(IN1R, OUTPUT);
-  pinMode(IN2R, OUTPUT);
-  pinMode(IN3R, OUTPUT);
-  pinMode(IN4R, OUTPUT);
-  pinMode(ENAR, OUTPUT);
-  pinMode(ENBR, OUTPUT);
-  pinMode(IN1L, OUTPUT);
-  pinMode(IN2L, OUTPUT);
-  pinMode(IN3L, OUTPUT);
-  pinMode(IN4L, OUTPUT);
-  pinMode(ENAL, OUTPUT);
-  pinMode(ENBL, OUTPUT);
-
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting to network...");
-
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(100);
-  }
-  Serial.println("\nConnected to network!:");
-  Serial.println(WiFi.localIP());
-
-  while(!client.connect(IPAddress(192,168,17,151), 10000)){
-    Serial.println("Connection to host failed");
-    delay(1000);
-  }
-  Serial.println("Connected to server!");
+  Serial.setTimeout(100);
 }
 
-void loop() {
-  if (!client.connected()){
-    Stop();
-    Serial.println("Reonnecting to server...");
-    while (!client.connect(IPAddress(192, 168, 17, 151), 10000)) {
-      Serial.println("Reconnection failed, retrying...");
-      delay(2000);
+void readSerial()
+{
+  if (Serial.available())
+  {
+    String s = Serial.readStringUntil('\n');
+    s.trim();
+    char op = s[0];
+    Serial.println(op);
+
+    if (op == 'f')
+    {
+      MoveFwd(defaultSpeed);
+    }
+    else if (op == 'b')
+    {
+      MoveBack(defaultSpeed);
+    }
+    else if (op == 'l')
+    {
+      MoveLeft(defaultSpeed);
+    }
+    else if (op == 'r')
+    {
+      MoveRight(defaultSpeed);
+    }
+    else if (op == 's')
+    {
+      Stop();
     }
   }
-  if (client.available()){
-    char command = client.read();
-    Serial.print(command);
-  if (command == 'f'){
-    MoveFwd(defaultSpeed);
-  }
-  else if (command== 'b'){
-    MoveBack(defaultSpeed);
-  }
-  else if (command=='l'){
-    MoveLeft(defaultSpeed);
-  }
-  else if (command=='r'){
-    MoveRight(defaultSpeed);
-  }
-  else if (command=='s'){
-    Stop();
-  }
-  }
 }
 
+void loop()
+{
+  readSerial();
+}
